ed45864289c3566826282aabc94fa6be4c898355
Rreze <rreze@cybertekschool.com>
2022-10-20 22:09:26 +0200

    Committing divisibleby3515, codility and prime number tasks

---
+++ src/Numbers_Tasks/Divisibleby3515_Solution2.java
@@ -0,0 +1,46 @@
+package Numbers_Tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Divisibleby3515_Solution2 {
+    public static void main(String[] args) {
+    divisible();
+    }
+    public static void divisible(){
+    String divisibleBy15 ="";
+
+    String divisibleBy5 ="";
+
+    String divisibleBy3="";
+
+    List<Integer> list = new ArrayList<Integer>();
+
+for(int i=0; i < 100; i++)
+
+            list.add(i+1);
+
+for(int each: list) {
+
+        if(each %15==0 && each %3==0)
+
+            divisibleBy15+= each+" ";
+
+        if(each %5==0 && each % 15!=0)
+
+            divisibleBy5 += each+" ";
+
+        if(each%3==0 && each %15!=0)
+
+            divisibleBy3 += each+" ";
+
+    }
+
+System.out.println("Divisible By 15: "+divisibleBy15);
+
+System.out.println("Divisible By 5: "+divisibleBy5);
+
+System.out.println("Divisible By 3: "+divisibleBy3);
+
+}
+}
---
+++ src/Numbers_Tasks/PrimeNumber.java
@@ -0,0 +1,23 @@
+package Numbers_Tasks;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println(primeNumber(5));
+        System.out.println(primeNumber(4));
+    }
+    public static boolean primeNumber(int num) {
+
+        if(num <= 1){
+            return false;
+        }
+
+        for(int i = 2; i < num; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
